0ac50e04c724305ac517cdb2735f0181ec8e1da2
Nagarehazh <proyectosexitososya@gmail.com>
2022-10-16 06:32:19 -0500

    Check for errors. All done

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '6be7b5a500msh52610ba34e87d3ep17d893jsn44727c4856f5',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = '8ce3e2ad5a1d6d91c323edcafb2fe60c';
+export const WEATHER_API_KEY = '';
